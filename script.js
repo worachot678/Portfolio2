08fe1ec46ea31a17fddcee7dc91cd3490fd1d741
worachot678 <16360@phanthong.ac.th>
2026-07-10 11:24:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to My Portfolio");
+
+document.querySelectorAll("nav a").forEach(link=>{
+    link.addEventListener("click",function(e){
+        e.preventDefault();
+
+        const target=document.querySelector(this.getAttribute("href"));
+
+        target.scrollIntoView({
+            behavior:"smooth"
+        });
+    });
+});
